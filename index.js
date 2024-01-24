7c01c594704d37d6ffcef1fccb5341ff4e335943
Corina Jewer <corina.jewer@keyin.com>
2024-01-24 12:50:38 -0330

    addes views & index.js along with code refractoring

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+global.DEBUG = true;
+ 
+const server = http.createServer((request, response) => {
+    if(DEBUG)console.log('Request Url:', request.url);
+    //let filename = 'index.html';
+    let path = './views/';
+
+    switch (request.url){
+        case '/':
+            if(DEBUG) console.log('Root Route')
+            path += 'index.html';
+            if(DEBUG) console.log('Path: ',path)
+            fetchFile(path);
+        break;
+
+        case '/about':
+            if(DEBUG) console.log('About Route')
+            //filename = 'about.html';
+            path += 'about.html';
+            //fetchFile(fiename);
+            if(DEBUG) console.log('Path: ',path);
+            fetchFile(path);
+        break;
+
+        case '/contact':
+            if(DEBUG) console.log('Contact Route')
+            //filename = 'contact.html';
+            path += 'contact.html';
+            //fetchFile(filename);
+            if(DEBUG) console.log('Path: ',path);
+            fetchFile(path);
+        break;
+
+        default:
+            if(DEBUG) console.log('404 not found')
+            response.writeHead(404, {'Content-Type':'text/plain'});
+            response.end('404 not found.')
+        break;
+    }  
+    function fetchFile(filename){
+        fs.readFile(filename,(error,content)=>{
+            if(error){
+            response.writeHead(500, {'Content-Type':"text/plain"});
+            response.end('500 Internal Server Error')
+            }else{
+            response.writeHead(200, {'Content-Type':"text/html"});
+            response.end(content,'utf-8');
+        }})
+    }     
+});
+
+server.listen(3000, () => {
+  console.log('Server is running...');
+});
